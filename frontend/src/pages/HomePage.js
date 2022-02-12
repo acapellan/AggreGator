216b5650e1f8e089dea2c740fb6443c029395bb2
Steven DeLoach <sfdeloach@gmail.com>
2022-02-12 10:54:34 -0500

    next lesson on React course

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    {/* <> is short for <React.Fragment> */}
+    <h1>Hello, welcome to AggreGator!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Nunc lobortis mattis aliquam faucibus purus in massa. Arcu ac
+      tortor dignissim convallis aenean. Curabitur vitae nunc sed velit. Adipiscing diam donec
+      adipiscing tristique risus nec feugiat in fermentum. Dui ut ornare lectus sit. Nisl
+      condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Dolor sit amet
+      consectetur adipiscing. Placerat in egestas erat imperdiet sed euismod nisi. Purus in massa
+      tempor nec feugiat nisl pretium fusce. Mi tempus imperdiet nulla malesuada pellentesque elit
+      eget gravida. Iaculis at erat pellentesque adipiscing. Tellus at urna condimentum mattis
+      pellentesque id nibh tortor id. Nisl vel pretium lectus quam id leo in. Vitae et leo duis ut.
+      Aliquam etiam erat velit scelerisque in dictum non. Ullamcorper a lacus vestibulum sed arcu
+      non. Adipiscing diam donec adipiscing tristique risus.
+    </p>
+    <code>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Nunc lobortis mattis aliquam faucibus purus in massa. Arcu ac
+      tortor dignissim convallis aenean. Curabitur vitae nunc sed velit. Adipiscing diam donec
+      adipiscing tristique risus nec feugiat in fermentum. Dui ut ornare lectus sit. Nisl
+      condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Dolor sit amet
+      consectetur adipiscing. Placerat in egestas erat imperdiet sed euismod nisi. Purus in massa
+      tempor nec feugiat nisl pretium fusce. Mi tempus imperdiet nulla malesuada pellentesque elit
+      eget gravida. Iaculis at erat pellentesque adipiscing. Tellus at urna condimentum mattis
+      pellentesque id nibh tortor id. Nisl vel pretium lectus quam id leo in. Vitae et leo duis ut.
+      Aliquam etiam erat velit scelerisque in dictum non. Ullamcorper a lacus vestibulum sed arcu
+      non. Adipiscing diam donec adipiscing tristique risus.
+    </code>
+  </> // </> is short for </React.Fragment>
+);
+
+export default HomePage;
