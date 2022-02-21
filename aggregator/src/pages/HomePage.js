15f050f0f4ef30adb85840c8b1874e60b297c197
Brian Hoblin <brian.hoblin@gmail.com>
2022-02-20 21:52:34 -0500

    PBI-ID-1.5 Created HomePage. Styling still needed

---
+++ aggregator/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const HomePage = () => {
+    return (
+        <>
+        <h1>Welcome to the AggreGator!</h1>
+        <p>
+            The AggreGator is a service to the University of Florida community to help perspective students, current students, alumni, and faculty find success in the classroom and beyond.
+        </p>
+        </>
+    )
+}
+
+export default HomePage;
