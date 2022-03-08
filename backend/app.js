3cf034ebca48afc6e76763682b03614267c000da
Steven DeLoach <sfdeloach@gmail.com>
2022-03-07 22:02:08 -0500

    added dummy keys

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const keys = require('./config/keys') || {google: {id: 'fake', secret: 'shh!'}};
 
 const app = express();
 
