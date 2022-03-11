c0b90f28131d9bdd8bd6972b7cf4c4929ffa29ec
Steven DeLoach <sfdeloach@gmail.com>
2022-03-10 20:42:47 -0500

    added mode messages

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,10 +1,13 @@
 // devKeys are not committed to repo, test keys are loaded to allow automated tests
 if (process.env.NODE_ENV === 'production') {
+  console.log('Production mode detected');
   module.exports = require('../config/prodKeys');
 } else {
   try {
+    console.log('Development mode detected');
     module.exports = require('../config/devKeys');
   } catch (err) {
+    console.log('Testing mode detected');
     module.exports = require('../config/testKeys');
   }
 }
