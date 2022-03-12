be7622be2ccd072f556791e5820cdfb4182a6603
Steven DeLoach <sfdeloach@gmail.com>
2022-03-12 11:02:35 -0500

    added react router

--- aggregator/src/App.js
+++ aggregator/src/App.js
@@ -1,10 +1,13 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
--- aggregator/src/pages/HomePage.js
+++ aggregator/src/pages/HomePage.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-export const HomePage = () => {
-    return (
-        <>
-        <h1>Welcome to the AggreGator!</h1>
-        <p>
-            The AggreGator is a service to the University of Florida community to help perspective students, current students, alumni, and faculty find success in the classroom and beyond.
-        </p>
-        </>
-    )
-}
+const HomePage = () => {
+  return (
+    <>
+      <h1>Welcome to the AggreGator!</h1>
+      <p>
+        The AggreGator is a service to the University of Florida community to help perspective
+        students, current students, alumni, and faculty find success in the classroom and beyond.
+      </p>
+    </>
+  );
+};
 
 export default HomePage;
