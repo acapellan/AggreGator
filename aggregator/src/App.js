117c706446bc19ab79d08e4a5b4f39118844dc88
Steven DeLoach <sfdeloach@gmail.com>
2022-03-18 17:01:05 -0400

    built latest app

--- aggregator/src/App.js
+++ aggregator/src/App.js
@@ -1,22 +1,24 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Topics from './components/Topics';
 import UserProfile from './components/UserProfile';
+
 import './App.scss';
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
+    <div>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/topics" element={<Topics />} />
           <Route path="/user-profile" element={<UserProfile />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 };
 
