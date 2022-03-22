69f9c4db971ccc636d7269b0950d291f81a0349d
Steven DeLoach <sfdeloach@gmail.com>
2022-03-21 23:02:38 -0400

    added redux thunk

--- aggregator/src/App.js
+++ aggregator/src/App.js
@@ -1,25 +1,36 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from './actions';
 
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Topics from './components/Topics';
+import NewTopic from './components/NewTopic';
 import UserProfile from './components/UserProfile';
 
 import './App.scss';
 
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/topics" element={<Topics />} />
-          <Route path="/user-profile" element={<UserProfile />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/topics" element={<Topics />} />
+            <Route path="/topics/new" element={<NewTopic />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- aggregator/src/components/Navbar.js
+++ aggregator/src/components/Navbar.js
@@ -1,47 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            AggreGator
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/topics">
-                  Topics
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/user-profile">
-                  User Profile
-                </a>
-              </li>
-            </ul>
+class Navbar extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'still deciding';
+      case false:
+        return 'logged out';
+      default:
+        return 'logged in';
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              AggreGator
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/topics">
+                    Topics
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/topics/new">
+                    New Topic
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/user-profile">
+                    User Profile
+                  </a>
+                </li>
+                {this.renderContent()}
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </>
-  );
-};
+        </nav>
+      </>
+    );
+  }
+}
 
-export default Navbar;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Navbar);
