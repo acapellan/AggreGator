52f911eef5325c99c0a0b33acf09f446032d1bb8
jacobboney <jacobaboney@gmail.com>
2022-03-27 14:30:40 -0400

    Made minor styling changes/reformats to landing page

--- aggregator/src/components/Landing.js
+++ aggregator/src/components/Landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import googleLogo from '../resources/images/googleLogo.png'
-import treeBackground from '../resources/images/background.png'
+import googleLogo from '../resources/images/googleLogo.png';
+import treeBackground from '../resources/images/background.png';
 
 const HomePage = () => {
   return (
@@ -14,19 +14,17 @@ const HomePage = () => {
             width="192"
             height="192"
           />
-          <h1 className="display-5 fw-bold" style={{color: 'white'}}>Welcome to AggreGator!</h1>
+          <h1 className="display-5 fw-bold text-white">Welcome to AggreGator!</h1>
           <div className="col-lg-12">
-            <p className="lead mb-4" style={{color: 'white'}}>
-              AggreGator is a service to the University of Florida community to help perspective
-              students, current students, alumni, and faculty find success in the classroom and
-              beyond.
+            <p className="lead mb-4 text-white">
+              AggreGator is a service to the University of Florida community to help perspective students, current students, alumni, and faculty find success in the classroom and beyond.
             </p>
             
           <div className="container">
             <div className="row my-2">
               <div className="col">
                   <a type="button" className="btn btn-primary btn-lg px-4 text-nowrap mx-2" href="/auth/current_user"style={{width: 165}}>Current User</a>
-                  <a type="button" className="btn btn-primary btn-lg px-4 text-nowrap mx-2" href="#" style={{width: 165}}>Sign Up</a>
+                  <a type="button" className="btn btn-primary btn-lg px-4 text-nowrap mx-2" href="/Signup" style={{width: 165}}>Sign Up</a>
               </div>
             </div>
             <div className="row my-2">
