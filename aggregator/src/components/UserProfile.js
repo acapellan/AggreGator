d077da1ebb01fceb7b35fa57081d12c6b2110b0c
jacobboney <jacobaboney@gmail.com>
2022-03-27 20:16:50 -0400

    Restyled pages to match sitewide design

--- aggregator/src/components/UserProfile.js
+++ aggregator/src/components/UserProfile.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import alligator_background from '../resources/images/alligator_background.png';
 
 const UserProfile = () => {
   return (
     <>
-      <div className="container">
-        <div className="px-4 py-5 my-5 text-center">
+      <div className="container-fluid" style={{backgroundImage:`url(${alligator_background})`, backgroundSize:"cover", backgroundRepeat:"repeat"}}>
+        <div className="px-4 py-5 text-center">
           <img
             className="d-block mx-auto mb-4"
             src="logo_square.png"
