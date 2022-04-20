908977eb16574aedf135466bc5f4a0d8df05ee9f
Steven DeLoach <sfdeloach@gmail.com>
2022-04-20 14:47:42 -0400

    update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,19 +1,15 @@
 // devKeys are not committed to repo
-const devKeys = require('./devKeys');
-const prodKeys = require('./prodKeys');
 // test keys are loaded to allow automated tests
-const testKeys = require('./testKeys');
-
 // production mode must also include a number of defined environment variables
 if (process.env.NODE_ENV === 'production') {
   console.log('Production mode detected');
-  module.exports = prodKeys;
+  module.exports = require('./prodKeys');
 } else {
   try {
     console.log('Development/Standalone/Local testing mode detected');
-    module.exports = devKeys;
+    module.exports = require('./devKeys');
   } catch (err) {
     console.log('Remote testing mode detected');
-    module.exports = testKeys;
+    module.exports = require('./testKeys');
   }
 }
