adcaa7c3d3402cd92ea4bf100df78fca27d26880
jacobboney <jacobaboney@gmail.com>
2022-04-20 21:01:58 -0400

    Reverted User Profile back to previous state

--- aggregator/src/components/UserProfile.js
+++ aggregator/src/components/UserProfile.js
@@ -1,70 +1,24 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-
+import React from 'react';
 import alligator_background from '../resources/images/alligator_background.png';
 import aggregatorLogo from '../resources/images/logo_square.png';
-import googleLogo from '../resources/images/googleLogo.png';
 
-class UserProfile extends Component {
-  renderContent() {
-    const user = this.props.auth;
-
-    switch (this.props.auth) {
-      case null:
-        <p className="text-white">Loading...</p>;
-      case false:
-        return (
-          <div className="col">
-            <h2 className="text-white my-2">Please Sign in to view your User Profile</h2>
-            <a
-            type="button"
-            className="btn btn-light btn-lg shadow-lg px-4 my-4"
-            href="/auth/google"
-            style={{ width: 255 }}
-          >
-            <img src={googleLogo} width="25" height="25" /> Sign in with Google
-          </a>
-          </div>
-          
-        );
-      default:
-        return (
-          <div className='col-12'>
-            <h2 className="text-white my-3">Hello, {user.name.first}!</h2>
-            <a type ="button" className="btn btn-info btn-lg shadow-lg px-4 my-3" href="/auth/logout">Logout</a>
-            </div>
-          
-        );
-    }
-  }
-
-  render() {
-      return (
-      <>
-        <div className="container-fluid" style={{backgroundImage:`url(${alligator_background})`, backgroundSize:"cover", backgroundRepeat:"repeat"}}>
-          <div className="text-center">
-          <div className="row">
-                    <h1 className="display-5 fw-bold py-4 text-white shadow-lg" style={{backgroundColor:"#407db4"}}>User Profile</h1>
-          </div>
-
-          <div className="row justify-content-center">
-          
-            <div className="col-lg-3 bg-primary rounded-3 shadow-lg mt-5 mb-5 py-4">
-            <div className="col-12">{this.renderContent()}</div>
-            </div>
-
-          </div>
-
-          </div>
+const UserProfile = () => {
+  return (
+    <>
+      <div className="container-fluid" style={{backgroundImage:`url(${alligator_background})`, backgroundSize:"cover", backgroundRepeat:"repeat"}}>
+        <div className="px-4 py-5 text-center">
+          <img
+            className="d-block mx-auto mb-4 rounded shadow-lg"
+            src={aggregatorLogo}
+            alt=""
+            width="192"
+            height="192"
+          />
+          <h1 className="display-5 fw-bold">User Profiles</h1>
         </div>
-      </>
-    );
-  } 
-}
+      </div>
+    </>
+  );
+};
 
-function mapStateToProps({ auth }) {
-  return { auth };
-}
-
-
-export default connect(mapStateToProps)(UserProfile);
+export default UserProfile;
