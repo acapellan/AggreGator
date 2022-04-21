6e26c3776e6f75bcfd1a64e6bc593c566b629457
jacobboney <jacobaboney@gmail.com>
2022-04-20 21:09:13 -0400

    Style changes to user profile page

--- aggregator/src/components/UserProfile.js
+++ aggregator/src/components/UserProfile.js
@@ -6,15 +6,21 @@ const UserProfile = () => {
   return (
     <>
       <div className="container-fluid" style={{backgroundImage:`url(${alligator_background})`, backgroundSize:"cover", backgroundRepeat:"repeat"}}>
-        <div className="px-4 py-5 text-center">
-          <img
-            className="d-block mx-auto mb-4 rounded shadow-lg"
-            src={aggregatorLogo}
-            alt=""
-            width="192"
-            height="192"
-          />
-          <h1 className="display-5 fw-bold">User Profiles</h1>
+        <div className="text-center">
+          <div className="row">
+            <h1 className="display-5 fw-bold mb-5 py-4 text-white shadow-lg" style={{backgroundColor:"#407db4"}}>User Profiles</h1>
+          </div>
+          <div className="row">
+            <div className="col mb-5">
+              <img
+              className="d-block mx-auto rounded shadow-lg"
+              src={aggregatorLogo}
+              alt=""
+              width="192"
+              height="192"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
